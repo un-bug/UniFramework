0eda914f0aaf6195129d9296272209cffb7c1a44
un-bug <xiangqiusheng@gmail.com>
2026-01-23 09:56:10 +0800

    重构 AssetLoaderDefineMenu.cs

--- Editor/MenuOptions/AssetLoaderDefineMenu.cs
+++ Editor/MenuOptions/AssetLoaderDefineMenu.cs
@@ -7,68 +7,77 @@ namespace UniFramework.Editor
 {
     public static class AssetLoaderDefineMenu
     {
-        private const string Define = "ENABLE_ADDRESSABLES";
+        private const string DefineSymbol = "ENABLE_ADDRESSABLES";
         private const string MenuRoot = "UniFramework/Asset Loader Mode/";
-        private const string MenuResources = MenuRoot + "Resources";
-        private const string MenuAddressables = MenuRoot + "Addressables";
 
-        private static readonly BuildTargetGroup[] BuildTargetGroups = new BuildTargetGroup[]
+        private static readonly BuildTargetGroup[] TargetGroups = new BuildTargetGroup[]
         {
             BuildTargetGroup.Standalone,
             BuildTargetGroup.iOS,
             BuildTargetGroup.Android
         };
 
-        [MenuItem(MenuResources, priority = 1)]
-        private static void UseResources() => SetDefine(false);
+        [MenuItem(MenuRoot + "Resources", priority = 1)]
+        private static void SwitchToResources() => UpdateDefines(false);
 
-        [MenuItem(MenuAddressables, priority = 2)]
-        private static void UseAddressables() => SetDefine(true);
+        [MenuItem(MenuRoot + "Addressables", priority = 2)]
+        private static void SwitchToAddressables() => UpdateDefines(true);
 
-        [MenuItem(MenuResources, true)]
+        [MenuItem(MenuRoot + "Resources", true)]
         private static bool ValidateResources()
         {
-            Menu.SetChecked(MenuResources, !HasDefine());
+            Menu.SetChecked(MenuRoot + "Resources", !IsAddressablesEnabled());
             return true;
         }
 
-        [MenuItem(MenuAddressables, true)]
+        [MenuItem(MenuRoot + "Addressables", true)]
         private static bool ValidateAddressables()
         {
-            Menu.SetChecked(MenuAddressables, HasDefine());
+            Menu.SetChecked(MenuRoot + "Addressables", IsAddressablesEnabled());
             return true;
         }
 
-        private static void SetDefine(bool enable)
+        private static void UpdateDefines(bool enableAddressables)
         {
-            foreach (BuildTargetGroup group in BuildTargetGroups)
-            {
-                if (group == BuildTargetGroup.Unknown)
-                {
-                    continue;
-                }
+            bool changed = false;
 
-                HashSet<string> symbols = new HashSet<string>(PlayerSettings.GetScriptingDefineSymbolsForGroup(group).Split(';'));
-                if (enable)
+            foreach (var group in TargetGroups)
+            {
+                if (group == BuildTargetGroup.Unknown) continue;
+
+                string currentDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
+                var symbols = new HashSet<string>(currentDefines.Split(';').Where(s => !string.IsNullOrWhiteSpace(s)));
+
+                if (enableAddressables)
                 {
-                    symbols.Add(Define);
+                    if (symbols.Add(DefineSymbol)) changed = true;
                 }
                 else
                 {
-                    symbols.Remove(Define);
+                    if (symbols.Remove(DefineSymbol)) changed = true;
                 }
 
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", symbols));
+                if (changed)
+                {
+                    PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", symbols));
+                }
             }
 
-            Debug.Log($"Asset Loader Mode => {(enable ? "Addressables" : "Resources")}");
-            AssetDatabase.Refresh();
+            if (changed)
+            {
+                Debug.Log($"[AssetLoader] Switched to: {(enableAddressables ? "Addressables" : "Resources")}");
+            }
+            else
+            {
+                Debug.Log($"[AssetLoader] Already in {(enableAddressables ? "Addressables" : "Resources")} mode.");
+            }
         }
 
-        private static bool HasDefine()
+        private static bool IsAddressablesEnabled()
         {
             var group = EditorUserBuildSettings.selectedBuildTargetGroup;
-            return PlayerSettings.GetScriptingDefineSymbolsForGroup(group).Split(';').Contains(Define);
+            var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
+            return symbols.Split(';').Contains(DefineSymbol);
         }
     }
 }
