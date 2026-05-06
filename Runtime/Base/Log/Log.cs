b4bcf9da969caf3fdf5a4aad489a59a6f19a5ecf
unbug <xiangqiusheng@gmail.com>
2026-05-07 00:54:29 +0800

    优化日志类

--- Runtime/Base/Log/Log.cs
+++ Runtime/Base/Log/Log.cs
@@ -1,30 +1,44 @@
 using System;
+using System.Diagnostics;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace UniFramework
 {
     public static class Log
     {
-        public const string Prefix = "[UniFramework]";
+        [Conditional("UNITY_EDITOR")]
+        [Conditional("DEVELOPMENT_BUILD")]
+        public static void Debug(string message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        [Conditional("DEVELOPMENT_BUILD")]
+        public static void Debug(string message, Object context)
+        {
+            UnityEngine.Debug.Log(message, context);
+        }
 
         public static void Info(string message)
         {
-            Debug.Log($"{Prefix} {message}");
+            UnityEngine.Debug.Log(message);
         }
 
         public static void Warning(string message)
         {
-            Debug.LogWarning($"{Prefix} {message}");
+            UnityEngine.Debug.LogWarning(message);
         }
 
         public static void Error(string message)
         {
-            Debug.LogError($"{Prefix} {message}");
+            UnityEngine.Debug.LogError(message);
         }
 
         public static void Exception(Exception exception)
         {
-            Debug.LogException(exception);
+            UnityEngine.Debug.LogException(exception);
         }
     }
 }
