32032f771b1d03d4703d9594ebe8a7643c83c13f
unbug <xiangqiusheng@gmail.com>
2026-05-07 00:32:13 +0800

    修改 MonoSingleton 单例实现

--- Runtime/Base/Singleton/MonoSingleton.cs
+++ Runtime/Base/Singleton/MonoSingleton.cs
@@ -4,54 +4,52 @@ namespace UniFramework
 {
     public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
-        protected static GameObject s_Root;
-        private static bool s_IsShutDown = false;
+        private const string RootName = "[MonoSingleton]";
+        private static bool s_IsQuitting;
         private static T s_Instance;
+        protected static GameObject s_Root;
 
+        protected virtual bool IsDontDestroyOnLoad => true;
+        public static bool HasInstance => s_Instance != null;
         public static T Instance
         {
             get
             {
-                if (s_Instance != null)
+                if (s_IsQuitting)
                 {
-                    return s_Instance;
-                }
-
-                s_Instance = FindObjectOfType<T>();
-
-                if (s_Instance != null)
-                {
-                    return s_Instance;
-                }
-
-                if (s_IsShutDown)
-                {
-                    Debug.LogWarning($"[MonoSingleton] instance of {typeof(T).Name} already destroyed. returning null.");
+                    Debug.LogWarning($"[MonoSingleton] instance of {typeof(T).Name} requested while application is quitting.");
                     return null;
                 }
 
-                if (s_Root == null)
-                {
-                    s_Root = GameObject.Find("[MonoSingleton]") ?? new GameObject("[MonoSingleton]");
-                    DontDestroyOnLoad(s_Root);
-                }
-
-                GameObject go = new GameObject($"[{typeof(T).Name}]");
-                go.transform.SetParent(s_Root.transform);
-                s_Instance = go.AddComponent<T>();
-                return s_Instance;
+                return GetOrCreateInstance();
             }
         }
 
         private void Awake()
         {
-            if (s_Instance && s_Instance != this)
+            if (s_Instance != null && s_Instance != this)
             {
+                Debug.LogWarning($"[MonoSingleton] duplicate instance of {typeof(T).Name} destroyed.", gameObject);
                 Destroy(gameObject);
                 return;
             }
 
             s_Instance = this as T;
+
+            if (IsDontDestroyOnLoad)
+            {
+                if (s_Root == null)
+                {
+                    s_Root = GameObject.Find(RootName) ?? new GameObject(RootName);
+                    DontDestroyOnLoad(s_Root);
+                }
+
+                if (transform.parent != s_Root.transform)
+                {
+                    transform.SetParent(s_Root.transform);
+                }
+            }
+
             Debug.Log($"[MonoSingleton] {typeof(T).Name} created.", gameObject);
             OnInit();
         }
@@ -68,7 +66,7 @@ namespace UniFramework
 
         private void OnApplicationQuit()
         {
-            s_IsShutDown = true;
+            s_IsQuitting = true;
         }
 
         private void Update()
@@ -84,5 +82,24 @@ namespace UniFramework
 
         protected virtual void OnUpdate(float deltaTime)
         { }
+
+        private static T GetOrCreateInstance()
+        {
+            if (s_Instance != null)
+            {
+                return s_Instance;
+            }
+
+            s_Instance = FindFirstObjectByType<T>();
+
+            if (s_Instance != null)
+            {
+                return s_Instance;
+            }
+
+            GameObject gameObj = new GameObject($"[{typeof(T).Name}]");
+            s_Instance = gameObj.AddComponent<T>();
+            return s_Instance;
+        }
     }
 }
