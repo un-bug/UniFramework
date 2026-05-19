b8814d54c86c950240e33feaba5ef867af8cdabd
unbug <xiangqiusheng@gmail.com>
2026-05-19 14:07:14 +0800

    拆分 MonoSingletonRuntime 静态类到独立文件

--- Runtime/Base/Singleton/MonoSingleton.cs
+++ Runtime/Base/Singleton/MonoSingleton.cs
@@ -2,46 +2,6 @@ using UnityEngine;
 
 namespace UniFramework
 {
-    internal static class MonoSingletonRuntime
-    {
-        public const string RootName = "[MonoSingleton]";
-        private static GameObject s_Root;
-        public static bool IsQuitting { get; private set; }
-        public static Transform RootTransform
-        {
-            get
-            {
-                if (s_Root == null)
-                {
-                    s_Root = GameObject.Find(RootName);
-
-                    if (s_Root == null)
-                    {
-                        s_Root = new GameObject(RootName);
-                    }
-
-                    Object.DontDestroyOnLoad(s_Root);
-                }
-
-                return s_Root.transform;
-            }
-        }
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void ResetRuntimeState()
-        {
-            IsQuitting = false;
-
-            Application.quitting -= OnApplicationQuitting;
-            Application.quitting += OnApplicationQuitting;
-        }
-
-        private static void OnApplicationQuitting()
-        {
-            IsQuitting = true;
-        }
-    }
-
     public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
         private static T s_Instance;
